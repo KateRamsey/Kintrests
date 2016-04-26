e5df3128244c64c1bbe300b81083e6a041f894e9
Kate Ramsey <kateramsey@live.com>
2016-04-26 13:42:37 -0500

    Initial Commit

---
+++ Kintrests/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kintrests.Startup))]
+namespace Kintrests
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
